fe59d35fedffa07d481ed94e98d7c0d5968038d1
gary <yellow90249@gmail.com>
2024-04-12 11:10:30 +0800

    feat: update

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
+app.get('/apple', (req, res) => {
+  res.send('cool apple');
+});
+
+app.get('/count', (req, res) => {
+  res.send('sexy count');
+});
+
 app.listen(3000, () => {
   console.log('Server starts listening on port 3000');
 });
